69b4996c80b2c99b22f5f1ad2d304fdc70f41e10
Brian Grant and Liam Ellis <brian+liam@>
2016-07-18 10:20:16 -0700

    databases connected, returning employees and divisions

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,6 +3,7 @@ describe(Employee) do
     it("tells which dvision an employee is in") do
       test_division = Division.create({:name => 'Human Resources'})
       test_employee = Employee.create({:first_name => "Jane", :last_name => "Doe", :division_id => test_division.id(), :project_id => nil})
+      expect(test_employee.division()).to(eq(test_division))
     end
   end
 end
